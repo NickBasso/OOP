c6e333555d198ef73862202ba567f4b29a4b79a0
Nicolae Basso <nickbassotrue@gmail.com>
2020-10-31 14:29:09 +0200

    LAB5 complete

--- POO-LAB5/src/base/Main.java
+++ POO-LAB5/src/base/Main.java
@@ -1,12 +1,21 @@
 package base;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
+    @Override
+    public String toString() {
+        return "Main{}";
+    }
 
     public static void main(String[] args) throws IllegalAccessException {
-	    A a = new A("a");
+	    A a = new A("a", new X("X"));
 	    B b = new B("a","b");
 	    C c = new C("a", "b", "c");
 	    D d = new D("a", "b", "c", "d");
@@ -16,6 +25,7 @@ public class Main {
 	    H h = new H("a", "b", "c", "d", "e", "f", "g", "h");
 	    I i = new I("a", "b", "c", "d", "e", "f", "g", "h", "i");
 	    J j = new J("a", "b", "c", "d", "e", "f", "g", "h", "i", "j");
+        //System.out.println(j.funct());
 
         ArrayList<Object> objects = new ArrayList<>();
         objects.add(a);
@@ -30,28 +40,36 @@ public class Main {
         objects.add(j);
 
         System.out.println("Instantiated objects' class name and all property fields (both inherited & declared):");
-        for(Object obj: objects){
-            /*Field[] fields = obj.getClass().getFields();
-
-            System.out.print(obj.getClass().getName() + " class object fields: ");
-            for(Field field: fields)
-                System.out.print(field.getName() + " " + field.get(field));
-
-            System.out.println();*/
-
+        for(Object obj: objects)
             System.out.println(obj.toString());
-        }
-
     }
 
     static class A {
-        public String a;
+        protected String a;
+        protected X x = new X("x A");
 
-        A(String a){
+        A(String a, X x){
             this.a = a;
         }
 
+        /*
+        // get all public & protected fields of a given class and its superclasses
+        List<Field> getAllFields(Class obj) {
+            if (obj == null)
+                return Collections.emptyList();
+
+            List<Field> res = new ArrayList<>(getAllFields(obj.getSuperclass()));
+            List<Field> filteredFields = Arrays.stream(obj.getDeclaredFields())
+                    .filter(f -> Modifier.isPublic(f.getModifiers()) || Modifier.isProtected(f.getModifiers()))
+                    .collect(Collectors.toList());
+
+            res.addAll(filteredFields);
+
+            return res;
+        }
+
         @Override
+        // return all public & protected fields of a given class and its superclasses as a String
         public String toString(){
             StringBuilder result = new StringBuilder();
             String newLine = System.getProperty("line.separator");
@@ -62,9 +80,10 @@ public class Main {
 
             //determine fields declared in this class only (no fields of superclass)
             Field[] fields = this.getClass().getFields();
+            List<Field> fieldss = getAllFields(this.getClass());
 
             //print field names paired with their values
-            for ( Field field : fields  ) {
+            for ( Field field : fieldss  ) {
                 result.append("  ");
                 try {
                     result.append( field.getName() );
@@ -80,86 +99,185 @@ public class Main {
 
             return result.toString();
         }
-    }
+        */
 
-   static class B extends A {
-        public String b;
-
-        B(String a, String b){
-            super(a);
-            this.b = b;
+        @Override
+        public String toString() {
+            return "A{" +
+                    "a='" + a + '\'' +
+                    ", x=" + x +
+                    '}';
         }
     }
 
+   static class B extends A {
+        protected String b;
+
+        B(String a, String b){
+            super(a, new X(new String("X")));
+            this.b = b;
+        }
+
+       @Override
+       public String toString() {
+           return "B{" +
+                   "b='" + b + '\'' +
+                   "} " + super.toString();
+       }
+   }
+
    static class C extends B {
-       public String c;
+       protected String c;
 
         C(String a, String b, String c){
             super(a, b);
             this.c = c;
         }
-    }
+
+       @Override
+       public String toString() {
+           return "C{" +
+                   "c='" + c + '\'' +
+                   "} " + super.toString();
+       }
+   }
 
    static class D extends C {
-       public String d;
+       protected String d;
+       protected X x = new X("x D");
 
-        D(String a, String b, String c, String d){
+       D(String a, String b, String c, String d){
             super(a, b, c);
             this.d = d;
-        }
-    }
+       }
+
+       @Override
+       public String toString() {
+           return "D{" +
+                   "d='" + d + '\'' +
+                   ", x=" + x +
+                   "} " + super.toString();
+       }
+   }
 
    static class E extends D {
-       public String e;
+       protected String e;
 
-        E(String a, String b, String c, String d, String e){
+       E(String a, String b, String c, String d, String e){
             super(a, b, c, d);
             this.e = e;
         }
-    }
+
+       @Override
+       public String toString() {
+           return "E{" +
+                   "e='" + e + '\'' +
+                   "} " + super.toString();
+       }
+   }
 
    static class F extends E {
-       public String f;
+       protected String f;
 
         F(String a, String b, String c, String d, String e, String f){
             super(a, b, c, d, e);
             this.f = f;
         }
-    }
+
+       @Override
+       public String toString() {
+           return "F{" +
+                   "f='" + f + '\'' +
+                   "} " + super.toString();
+       }
+   }
 
    static class G extends F {
-       public String g;
+       protected String g;
 
         G(String a, String b, String c, String d, String e, String f, String g){
             super(a, b, c, d, e, f);
             this.g = g;
         }
-    }
+
+       @Override
+       public String toString() {
+           return "G{" +
+                   "g='" + g + '\'' +
+                   "} " + super.toString();
+       }
+   }
 
    static class H extends G {
-       public String h;
+       protected String h;
+       private X x = new X(new String("x H"));
 
         H(String a, String b, String c, String d, String e, String f, String g, String h){
             super(a, b, c, d, e, f, g);
             this.h = h;
         }
-    }
+
+       @Override
+       public String toString() {
+           return "H{" +
+                   "h='" + h + '\'' +
+                   ", x=" + x +
+                   "} " + super.toString();
+       }
+   }
 
    static class I extends H {
-       public String i;
+       protected String i;
 
         I(String a, String b, String c, String d, String e, String f, String g, String h, String i){
             super(a, b, c, d, e, f, g, h);
             this.i = i;
         }
-    }
+
+       @Override
+       public String toString() {
+           return "I{" +
+                   "i='" + i + '\'' +
+                   "} " + super.toString();
+       }
+   }
 
    static class J extends I {
-       public String j;
+       protected String j;
 
-        J(String a, String b, String c, String d, String e, String f, String g, String h, String i, String j){
-            super(a, b, c, d, e, f, g, h, i);
-            this.j = j;
-        }
-    }
+       J(String a, String b, String c, String d, String e, String f, String g, String h, String i, String j) {
+           super(a, b, c, d, e, f, g, h, i);
+           this.j = j;
+       }
+
+       @Override
+       public String toString() {
+           return "J{" +
+                   "j='" + j + '\'' +
+                   "} " + super.toString();
+       }
+
+       /*public String funct(){
+           return this.x + " " + this.b + " " + this.d + " " + this.h + " " +  this.j;
+       }*/
+   }
+
+   static class X {
+       private String name;
+
+       X(String name){
+           this.name = name;
+       }
+
+       protected String getName(){
+           return this.name;
+       }
+
+       @Override
+       public String toString() {
+           return "X{" +
+                   "name='" + name + '\'' +
+                   '}';
+       }
+   }
 }
